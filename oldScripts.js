43bf4678f1d63aa4cd5e59bb81c274311e6a45e7
Christopher M Zeltner <zeltner12345@gmail.com>
2022-08-05 14:36:14 -0400

    Changed some js logic, might try to reduce lines. Basically done at this point

--- oldScripts.js
+++ oldScripts.js
@@ -38,110 +38,8 @@ window.onload = function() {
 }
 
 
-//////////////////////////////////////////////////////////////////////
-//                        Cart Functions
-//////////////////////////////////////////////////////////////////////
 
-
-//Increments/Decrements order quantity
-function handleQuantity(action) {
-    let num = parseInt(document.getElementById('quantity').innerHTML)
-    if(action === -1 && num > 0){
-        num -= 1
-    } else if (action === 1) {
-        num += 1
-    }
-    document.getElementById('quantity').innerHTML = num
-}
-    
-    
-function toggleCartModal() {
-    const modal = document.getElementById('cart-modal')
-    if(document.getElementById('cart-modal').style.display === 'block'){
-        document.getElementById('cart-modal').style.display = 'none'
-    } else{
-        document.getElementById('cart-modal').style.display = 'block'
-    }
-}
-    
-    
-function addToCart(){
-    createCartEntry()
-    
-    const modal = document.getElementById('cart-modal')
-    
-    //Show "Checkout" button if this is the first cart item added
-    document.getElementById('checkout-button').style.display = 'block'
-}
-    
-        
-function createCartEntry() {
-    const modal = document.getElementById('cart-item-container')
-    
-    //Get Price and Product info for template literals
-    const quantity = parseInt(document.getElementById('quantity').innerHTML)
-    const price = parseInt(document.getElementById('sale-price').children[0].innerHTML)
-    const total = quantity * price
-    const name = document.querySelector('h1').innerHTML
-    
-    document.getElementById('cart-empty').style.display = 'none'
-    
-    let id = getID()
-    
-    
-    //If there's a smarter/more eloquent way of doing this please let me know
-    modal.innerHTML += `<div class='modal-main' id='${id}'>
-    <img src="images/image-product-1-thumbnail.jpg" alt="" class='cart-thumbnail'>
-    <div class='modal-product-info'>
-    <span class='item-name'>${name}</span>
-    <div class='math-total-price-container'>
-        <span class='math'>$${price}.00 x ${quantity}</span>
-    <span class='total-price'>$${total}.00</span>
-    </div>
-    </div>
-    <img src="images/icon-delete.svg" alt="" class='trash-icon' onClick=deleteItem(${id})>
-    </div>`
-    
-    const cartQuantity = document.getElementById('cart-quantity')
-    cartQuantity.style.display = 'inline'
-    cartQuantity.innerHTML = id
-}
-    
-
-//Gets next ID for the cart item (ID is used for deletion)
-function getID() {
-    const items = document.querySelectorAll('.modal-main')
-    try {
-        const id = parseInt(items[items.length - 1].id)
-        return id + 1
-    } catch (error) {
-        return 1
-    } 
-}
-    
-    
-function deleteItem(id){
-    const item = document.getElementById(id)
-    item.remove()
-    
-    const containerEmpty = document.getElementById('cart-item-container')
-    if(containerEmpty.childNodes.length === 0){
-        document.getElementById('cart-empty').style.display = 'block'
-        document.getElementById('checkout-button').style.display = 'none'
-        document.getElementById('cart-quantity').style.display = 'none'
-    } else {
-        document.getElementById('cart-quantity').innerHTML = document.querySelectorAll('.modal-main').length
-    }
-    
-}
-
-
-//////////////////////////////////////////////////////////////////////
-//                        Image Functions (non-modal)
-//////////////////////////////////////////////////////////////////////
-
-
-//Functionality for navigation buttons on mobile screen
+//Slightly different on mobile
 function prevNextMobile(direction){
 
     let index = getIndex() + direction
@@ -173,6 +71,8 @@ function prevNextMobile(direction){
 }
 
 
+
+
 //When thumbnail is clicked, thumbnail is "selected" and corresponding image is displayed 
 function displayThumbnailImage(index){
     //Displays selected image, hides previous
@@ -193,46 +93,12 @@ function displayThumbnailImage(index){
 }
 
 
-//Gets index of current selected image
-function getIndex(){
-    const images = document.querySelectorAll('.image-full, .image-full-selected')
-    let i = 0
-    while(i < images.length){
-        if(images[i].className === 'image-full-selected'){
-            return i
-        }
-        i++
-    }    
-}
-
-
-//Used in conjunction with getIndex to set the modal image to the corresponding carousel image
-function setModal(index){
-    const modalIMGs = document.querySelectorAll('.image-full-modal, .image-full-modal-selected')
-    modalIMGs.forEach(img => {
-        img.className = 'image-full-modal'
-    })
-    modalIMGs[index].className = 'image-full-modal-selected'
-
-    const thumbnails = document.querySelectorAll('.image-thumbnail-modal, .image-thumbnail-modal-selected')
-    thumbnails.forEach(thumbnail => {
-        thumbnail.className = 'image-thumbnail-modal'
-    })
-    thumbnails[index].className = 'image-thumbnail-modal-selected'
-}
-
-
-//////////////////////////////////////////////////////////////////////
-//                        Modal Functions
-//////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 
 
 function openImageModal() {
-    const index = getIndex()
-    setModal(index)
     document.getElementById('image-carousel-modal').style.display = 'flex'
 }
-
 
 //Previous and next button functionality for image modal
 function prevNextModal(direction){
@@ -270,21 +136,37 @@ function displayThumbnailImageModal(index){
     images.forEach(image => {
         image.className = 'image-full-modal'
     })
-    images[index].className = 'image-full-modal-selected'
+    images[index - 1].className = 'image-full-modal-selected'
 
     //Applies selected class to the selected thumbnail
     const thumbnails = document.querySelectorAll('.image-thumbnail-modal-selected, .image-thumbnail-modal')
     thumbnails.forEach(thumbnail => {
         thumbnail.className = 'image-thumbnail-modal'
     })
-    thumbnails[index].className = 'image-thumbnail-modal-selected'
+    thumbnails[index - 1].className = 'image-thumbnail-modal-selected'
 }
+
 
 
 function closeModal(){
     document.getElementById('image-carousel-modal').style.display = 'none'
+    
 }
 
+//////////////////////////////////////////////////////////////////////
+//                        Modal Functions
+//////////////////////////////////////////////////////////////////////
+
+function getIndex(){
+    const images = document.querySelectorAll('.image-full, .image-full-selected')
+    let i = 0
+    while(i < images.length){
+        if(images[i].className === 'image-full-selected'){
+            return i
+        }
+        i++
+    }    
+}
 
 function getIndexModal(){
     const images = document.querySelectorAll('.image-full-modal, .image-full-modal-selected')
@@ -297,10 +179,20 @@ function getIndexModal(){
     }    
 }
 
+function setModal(index){
+    const modalIMGs = document.querySelectorAll('.image-full-modal, .image-full-modal-selected')
+    modalIMGs.forEach(img => {
+        img.className = 'image-full-modal'
+    })
+    modalIMGs[index].className = 'image-full-modal-selected'
 
-//////////////////////////////////////////////////////////////////////
-//                        Menu Functions
-//////////////////////////////////////////////////////////////////////
+    const thumbnails = document.querySelectorAll('.image-thumbnail-modal, .image-thumbnail-modal-selected')
+    thumbnails.forEach(thumbnail => {
+        thumbnail.className = 'image-thumbnail-modal'
+    })
+    thumbnails[index].className = 'image-thumbnail-modal-selected'
+}
+
 
 
 function toggleMenu() {
@@ -312,3 +204,4 @@ function toggleMenu() {
         document.getElementById('transparent-background').style.display = 'block'
     }
 }
+
